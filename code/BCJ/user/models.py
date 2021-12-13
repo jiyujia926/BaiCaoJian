b793231639a27c0fc2b4f69ecb29bc67009fa5ae
bailiao <3190105008@zju.edu.cn>
2021-12-13 21:29:47 +0800

    Update models.py

--- code/BCJ/user/models.py
+++ code/BCJ/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class User(models.Model):
+    UID = models.UUIDField(max_length=15, primary_key=True, blank = False)
+    Name = models.CharField(max_length=10, blank=False)
+    Password = models.CharField(max_length=20, blank=False)
+    Email = models.EmailField(max_length=30)
+    Github = models.CharField(max_length=30)
