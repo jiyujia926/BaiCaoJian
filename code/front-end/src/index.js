df81c3266ff2938cc59e28a9a5015239f8e6f59f
Wu Xinberry <3190103044@zju.edu.cn>
2021-12-14 20:33:53 +0800

    show logo, layout index page 吴

--- code/front-end/src/index.js
+++ code/front-end/src/index.js
@@ -5,12 +5,32 @@ import { theme } from "./style.js";
 import Admin from "./admin.js";
 import Footer from "./components/footer/Footer";
 import SearchForm from "./components/searchBox/SearchBox";
+import LogoImg from "./images/logo.svg";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Admin />
-      <SearchForm />
+      <div style={{
+          width: "100%",
+          justifyContent: "center", 
+          alignItems: "center",
+          display: "flex",  
+          display: "-webkit-flex",
+        }}
+      >
+        <img src={LogoImg} alt="Logo"></img>
+      </div> 
+      <div style={{
+          width: "100%",
+          justifyContent: "center", 
+          alignItems: "center",
+          display: "flex",  
+          display: "-webkit-flex",
+        }}
+      >
+        <SearchForm />
+      </div>
       <Footer />
     </ThemeProvider>
   </React.StrictMode>,
