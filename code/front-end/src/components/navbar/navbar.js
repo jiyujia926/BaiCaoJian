c5b1bdfdba65c3722cae52c42d61acc2c2b19968
jiyujia926 <84773677+jiyujia926@users.noreply.github.com>
2021-12-15 18:26:46 +0800

    Update navbar.js

--- code/front-end/src/components/navbar/navbar.js
+++ code/front-end/src/components/navbar/navbar.js
@@ -27,7 +27,7 @@ import useStyles from "./styles";
 import axios from "axios";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post["Content-Type"] = "application/json";
-//const server = "http://122.51.228.166:8000";
+//const server = "http://1.15.97.64:6636";
 const server = "http://127.0.0.1:8000";
 
 const Navbar = () => {
